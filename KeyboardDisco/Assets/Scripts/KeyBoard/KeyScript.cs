bcc206f4412429b8a1320c5bb985606f8eccd51f
Parker <91403070+pdotis@users.noreply.github.com>
2025-04-24 11:27:01 +1000

    New grid system
    Made a grid system, with the keyboard movement

--- KeyboardDisco/Assets/Scripts/KeyBoard/KeyScript.cs
+++ KeyboardDisco/Assets/Scripts/KeyBoard/KeyScript.cs
@@ -1,131 +1,51 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class KeyScript : MonoBehaviour
 {
+	public KeyCode Key;
+	public KeyPos Pos;
 
-    private KeyState state;
+	private bool _highlighted = false;
 
-    public bool isStartKey;
+	public bool IsHighlighted
+	{
+		get
+		{
+			return _highlighted;
+		}
+		set
+		{
+			if (value == true)
+			{
+				// Make highlighted
+				_highlighted = true;
+				rend.sharedMaterial = Materials[1];
+			}
+			else
+			{
+				// Make not highlighted
+				_highlighted = false;
+				rend.sharedMaterial = Materials[0];
+			}
+		}
+	}
 
-    public KeyCode key;
+	public void Setup(KeyCode key, KeyPos pos)
+	{
+		Key = key;
+		Pos = pos;
+	}
 
-    public GameObject[] AdjacentKeys;
+	// 0 is default, 1 is Pressed, 2 is Unlocked, 3 is Locked Press
+	public Material[] Materials;
+	Renderer rend;
 
-    public Material[] Materials; // 0 is default, 1 is Pressed, 2 is Unlocked, 3 is Locked Press
-    Renderer rend;
-
-    public float LockDelay;
-
-    void Start()
-    {
-        rend = GetComponent<Renderer>();
-        rend.enabled = true;
-        rend.sharedMaterial = Materials[0];
-
-        state = KeyState.Idle;
-    }
-
-    void Update()
-    {
-        if (state != KeyState.Pressed &&isStartKey && !Input.anyKeyDown)
-        {
-            state = KeyState.Unlocked;
-        }
-
-        if (Input.GetKeyDown(key))
-        {
-            if(state == KeyState.Unlocked || state == KeyState.Pressed)
-            {
-                state = KeyState.Pressed;
-                //AdjacentUnlock();
-                Debug.Log("Pressed " + key);
-            }
-            else
-            {
-                state = KeyState.Locked;
-            }
-                
-            
-        }
-
-        if (Input.GetKeyUp(key))
-        {
-            if(state == KeyState.Pressed)
-            {
-                //Invoke("AdjacentLock", LockDelay);
-                state = KeyState.Unlocked;
-            }
-            state = KeyState.Idle;
-            Debug.Log("Released " + key);
-        }
-
-        if (state != KeyState.Pressed && !isStartKey)
-        {
-            state = KeyState.Idle;
-            if (state == KeyState.Idle && AdjacentCheck())
-            {
-                state = KeyState.Unlocked;
-            }
-        }
-
-        //color    
-        switch (state)
-        {
-            case KeyState.Idle:
-                rend.sharedMaterial = Materials[0];
-                break;
-
-            case KeyState.Pressed:
-                rend.sharedMaterial = Materials[1];
-                break;
-
-            case KeyState.Unlocked:
-                rend.sharedMaterial = Materials[2];
-                break;
-
-            case KeyState.Locked:
-                rend.sharedMaterial = Materials[3];
-                break;
-        }
-
-    }
-
-    private void AdjacentUnlock()
-    {
-        for (int i = 0; i < AdjacentKeys.Length; i++)
-        {
-            AdjacentKeys[i].GetComponent<KeyScript>().state = KeyState.Unlocked;
-        }
-    }
-
-    private void AdjacentLock()
-    {
-        for (int i = 0; i < AdjacentKeys.Length; i++)
-        {
-            if(AdjacentKeys[i].GetComponent<KeyScript>().state == KeyState.Unlocked)
-            {
-                AdjacentKeys[i].GetComponent<KeyScript>().state = KeyState.Idle;
-            }
-        }
-    }
-
-    private bool AdjacentCheck()
-    {
-        for (int i = 0; i < AdjacentKeys.Length; i++)
-        {
-            if (AdjacentKeys[i].GetComponent<KeyScript>().state == KeyState.Pressed)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    private enum KeyState
-    {
-        Idle,
-        Pressed,
-        Unlocked,
-        Locked
-    }
+	void Start()
+	{
+		rend = GetComponent<Renderer>();
+		rend.enabled = true;
+		rend.sharedMaterial = Materials[0];
+	}
 }
---
+++ KeyboardDisco/Assets/Scripts/KeyBoard/KeyboardScript.cs
@@ -0,0 +1,276 @@
+using UnityEngine;
+using TMPro;
+
+public struct KeyPos
+{
+	public int x, y;
+
+	private static int maxX0 = 13;
+	private static int maxX1 = 13;
+	private static int maxX2 = 12;
+	private static int maxX3 = 11;
+
+	public KeyPos(int _x, int _y)
+	{
+		x = _x;
+		y = _y;
+	}
+
+	public bool IsAdjacent(KeyPos pos)
+	{
+		if (pos.y == y)
+			if (pos.x + 1 == x || pos.x - 1 == x)
+				return true;
+		
+		if (pos.x == x)
+			if (pos.y + 1 == y || pos.y - 1 == y)
+				return true;
+
+		if (pos.y == y + 1)
+			if (pos.x == x - 1)
+				return true;
+
+		if (pos.y == y - 1)
+			if (pos.x == x + 1)
+				return true;
+
+		return false;
+	}
+
+	public KeyPos Left(int count = 1)
+	{
+		int newX = x - count;
+		if (newX < 0) newX = 0;
+		
+		return new KeyPos(newX, y);
+	}
+
+	public KeyPos Right(int count = 1)
+	{
+		int newX = x + count;
+		switch (y)
+		{
+			case 0:
+				if (newX > maxX0)
+					newX = maxX0;
+				break;
+			case 1:
+				if (newX > maxX1)
+					newX = maxX1;
+				break;
+			case 2:
+				if (newX > maxX2)
+					newX = maxX2;
+				break;
+			case 3:
+				if (newX > maxX3)
+					newX = maxX3;
+				break;
+		}
+		
+		return new KeyPos(newX, y);
+	}
+
+	public KeyPos UpLeft(int count = 1)
+	{
+		int newY = y - count;
+		if (newY < 0) newY = 0;
+
+		return new KeyPos(x, newY);
+	}
+
+	public KeyPos UpRight(int count = 1)
+	{
+		KeyPos newPos = new KeyPos(x, y);
+
+		for (int i = 0; i < count; i++)
+		{
+			newPos = newPos.UpLeft();
+			newPos = newPos.Right();
+		}
+
+		return newPos;
+	}
+
+	public KeyPos DownRight(int count = 1)
+	{
+		int newY = y + count;
+		if (newY > 3) newY = 3;
+
+		return new KeyPos(x, newY);
+	}
+
+	public KeyPos DownLeft(int count = 1)
+	{
+		KeyPos newPos = new KeyPos(x, y);
+
+		for (int i = 0; i < count; i++)
+		{
+			newPos = newPos.DownRight();
+			newPos = newPos.Left();
+		}
+
+		return newPos;
+	}
+}
+
+public class KeyboardScript : MonoBehaviour
+{
+	private KeyScript[][] keys;
+	private KeyCode[][] keycodes =
+	{
+		new KeyCode[] {
+			KeyCode.BackQuote,
+			KeyCode.Alpha1,
+			KeyCode.Alpha2,
+			KeyCode.Alpha3,
+			KeyCode.Alpha4,
+			KeyCode.Alpha5,
+			KeyCode.Alpha6,
+			KeyCode.Alpha7,
+			KeyCode.Alpha8,
+			KeyCode.Alpha9,
+			KeyCode.Alpha0,
+			KeyCode.Minus,
+			KeyCode.Equals,
+			KeyCode.Backspace
+		},
+		new KeyCode[] {
+			KeyCode.Tab,
+			KeyCode.Q,
+			KeyCode.W,
+			KeyCode.E,
+			KeyCode.R,
+			KeyCode.T,
+			KeyCode.Y,
+			KeyCode.U,
+			KeyCode.I,
+			KeyCode.O,
+			KeyCode.P,
+			KeyCode.LeftBracket,
+			KeyCode.RightBracket,
+			KeyCode.Backslash
+		},
+		new KeyCode[] {
+			KeyCode.CapsLock,
+			KeyCode.A,
+			KeyCode.S,
+			KeyCode.D,
+			KeyCode.F,
+			KeyCode.G,
+			KeyCode.H,
+			KeyCode.J,
+			KeyCode.K,
+			KeyCode.L,
+			KeyCode.Semicolon,
+			KeyCode.Quote,
+			KeyCode.Return
+		},
+		new KeyCode[] {
+			KeyCode.LeftShift,
+			KeyCode.Z,
+			KeyCode.X,
+			KeyCode.C,
+			KeyCode.V,
+			KeyCode.B,
+			KeyCode.N,
+			KeyCode.M,
+			KeyCode.Comma,
+			KeyCode.Period,
+			KeyCode.Slash,
+			KeyCode.RightShift
+		}
+	};
+	private KeyPos currentPos = new KeyPos(0, 0);
+
+	public void Start()
+	{
+		FindKeys();
+		//SetupKeys();
+	}
+
+	private void FindKeys()
+	{
+		int rowNumber = 0;
+		keys = new KeyScript[4][];
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			if (transform.GetChild(i).name == "row")
+			{
+				keys[rowNumber] = new KeyScript[transform.GetChild(i).childCount];
+				for (int j = 0; j < transform.GetChild(i).childCount; j++)
+				{
+					KeyScript newKey = transform.GetChild(i).GetChild(j).GetComponent<KeyScript>();
+					if (newKey != null)
+					{
+						keys[rowNumber][j] = newKey;
+						keys[rowNumber][j].Setup(keycodes[rowNumber][j], new KeyPos(rowNumber, j));
+					}
+				}
+				rowNumber++;
+			}
+		}
+	}
+
+	/*private void AddKey(KeyScript newKey)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 14; j++)
+			{
+				if (keycode[i][j] == newKey.)
+				keys[i][j] = newKey;
+			}
+		}
+	}
+
+	private void SetupKeys()
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 14; j++)
+			{
+				keys[i][j].Setup(keycodes[i][j], new KeyPos(i, j));
+			}
+		}
+	}*/
+
+	public void Update()
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 14; j++)
+			{
+				if (j >= keycodes[i].Length)
+					continue;
+				
+				KeyCode key = keys[i][j].Key;
+				if (Input.GetKeyDown(key))
+				{
+					KeyPos newPos = keys[i][j].Pos;
+					if (currentPos.IsAdjacent(newPos))
+					{
+						keys[currentPos.x][currentPos.y].IsHighlighted = false;
+						keys[newPos.x][newPos.y].IsHighlighted = true;
+						currentPos = newPos;
+					}
+				}
+			}
+		}
+	}
+
+	/*private KeyPos GetPosFromKeyCode(KeyCode key)
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 14; j++)
+			{
+				if (keys[i][j].Key == key)
+					return keys[i][j].Pos;
+			}
+		}
+
+		Debug.Log("Invalid keycode!");
+		return new KeyPos(0, 0);
+	}*/
+}
